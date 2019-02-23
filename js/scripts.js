a8d21c92954ff0eb0eba4815f834a2189cca3927
Stewart ilondanga <stilondanga@gmail.com>
2019-02-23 20:36:22 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,37 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+let currFontsize = document.querySelector('#elem');
+
+let currLineheight = document.querySelector('#elem span');
+
+let fontsizeElem = document.getElementById('font-size-1');
+let lineheightElem = document.getElementById('line-height-1');
+
+
+let fontsize = fontsizeElem.value;
+let lineheight = lineheightElem.value;
+
+fontsizeElem.addEventListener('change', () => {
+   currFontsize.style.setProperty("--font-size", fontsizeElem.value);
+});
+
+lineheightElem.addEventListener('change', () => {
+   currLineheight.style.setProperty("--line-height",  lineheightElem.value);
+});
+// set from ui the fontsize and line height to see the differences
+
+
+
+//console.log(fontsize.value);
+// get variable from inline style
+
+
+
+getComputedStyle(currFontsize).getPropertyValue("--line-height");
+
+//element.style.getPropertyValue("--line-height");
+
+// get variable from wherever
+//getComputedStyle(element).getPropertyValue("--my-var");
